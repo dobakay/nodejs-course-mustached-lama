b178ac6685df6c13d376d1b08222cfa5a1565d07
dobata <dkralev90@gmail.com>
2014-09-14 15:44:58 +0300

    Changing solution to beerFries exercise

--- course-entry/entry-solutions/beer-and-fries/beerAndFries.js
+++ course-entry/entry-solutions/beer-and-fries/beerAndFries.js
@@ -6,20 +6,42 @@ function isFries (item) {
     return item.type === 'fries';
 }
 
+function sortByScore(a, b) {
+    if (a.score > b.score) {
+        return 1;
+    }
+    if (a.score < b.score) {
+        return -1;
+    }
+    // a must be equal to b
+    return 0;
+}
+
+
 exports.beerAndFries = function (beerAndFriesArray) {
     'use strict';
     var beers = beerAndFriesArray.filter(isBeer),
         fries = beerAndFriesArray.filter(isFries),
-        bestBeerFriesCombo = 0;
+        beerFriesCombos = [],
+        bestComboSum = 0
 
+    beers.sort(sortByScore);
+    fries.sort(sortByScore);
+
+    //building combo matrix
     for (var i = 0; i < beers.length; i++) {
+        var temp = [];
         for (var j = 0; j < fries.length; j++) {
-            if(fries[j].score * beers[i].score > bestBeerFriesCombo) {
-                bestBeerFriesCombo = fries[j].score * beers[i].score;
-            }
+            temp.push(fries[j].score * beers[i].score);
         };
+        beerFriesCombos.push(temp);
     };
-    console.log(bestBeerFriesCombo);
-    return bestBeerFriesCombo;
+
+    for (var i = 0; i < beerFriesCombos.length; i++) {
+        bestComboSum += beerFriesCombos[i][i];
+    };
+
+
+    return bestComboSum;
 }
 
