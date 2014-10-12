51c3cc455626f6067e6a0d223eac4c17276bf88f
dobata <dkralev90@gmail.com>
2014-10-12 21:08:09 +0300

    Adding walter routing engine

---
+++ week0/chirper/walter/router.js
@@ -0,0 +1,141 @@
+var router = (function() {
+    'use strict';
+
+    var router = {};
+    var httpMethods = {
+        get: 'GET',
+        post: 'POST',
+        put: 'PUT',
+        delete: 'DELETE'
+    }
+    var requests = {};
+
+    ///////////////////////////////////
+    // Generic Router Exception Def  //
+    ///////////////////////////////////
+
+    function RouterException (message, innerType, internalMessage) {
+        this.type = 'RouterException';
+        this.innerType = innerType || 'innerServerException';
+        this.message = message;
+        this.internalMessage = internalMessage || null;
+    }
+
+    ////////////////////////
+    // Internal Functions //
+    ////////////////////////
+
+    /**
+     * Adds a HTTP Method with the corresponding, url the handler for it.
+     * There can be one HTTP Method with many urls.
+     * @param {GET/POST/PUT/DELETE}   method   HTTP Method
+     * @param {URL String}   url      URL String gotten from the request object
+     * @param {Function} callback Handler that makes a call to the "hard-core" logic of the server
+     * @param {Bool} async Defines where to store the callback function
+     */
+    function addMethod (method, url, callback, async) {
+        if(!requests[method]) {
+            requests[method] = {
+                callbacks: {},
+                asyncCallbacks: {}
+            }
+        }
+        if(!async) {
+            requests[method].callbacks[url] = callback;
+        }
+        else {
+            requests[method].asyncCallbacks[url] = callback;
+        }
+    }
+
+    //////////////////////////
+    // Router API functions //
+    //////////////////////////
+
+    // SYNC
+
+    router.get = function (url, callback) {
+        if(!url && !callback) {
+            throw new RouterException('Url, or callback is undefined or null.');
+        }
+        addMethod(httpMethods.get, url, callback);
+    }
+
+    router.post = function (url, callback) {
+        if(!url && !callback) {
+            throw new RouterException('Url, or callback is undefined or null.');
+        }
+        addMethod(httpMethods.post, url, callback);
+    }
+
+    router.put = function (url, callback) {
+        if(!url && !callback) {
+            throw new RouterException('Url, or callback is undefined or null.');
+        }
+        addMethod(httpMethods.put, url, callback);
+    }
+
+    router.delete = function (url, callback) {
+        if(!url && !callback) {
+            throw new RouterException('Url, or callback is undefined or null.');
+        }
+        addMethod(httpMethods.delete, url, callback);
+    }
+
+    // ASYNC
+
+    router.getAsync = function (url, callback) {
+        if(!url && !callback) {
+            throw new RouterException('Url, or callback is undefined or null.');
+        }
+        addMethod(httpMethods.get, url, callback, true);
+    }
+
+    router.postAsync = function (url, callback) {
+        if(!url && !callback) {
+            throw new RouterException('Url, or callback is undefined or null.');
+        }
+        addMethod(httpMethods.post, url, callback, true);
+    }
+
+    router.putAsync = function (url, callback) {
+        if(!url && !callback) {
+            throw new RouterException('Url, or callback is undefined or null.');
+        }
+        addMethod(httpMethods.put, url, callback, true);
+    }
+
+    router.deleteAsync = function (url, callback) {
+        if(!url && !callback) {
+            throw new RouterException('Url, or callback is undefined or null.');
+        }
+        addMethod(httpMethods.delete, url, callback, true);
+    }
+
+        /**
+     * Executes one of the predefined actions depending on the given parameters object in the process function.
+     * @param  {[type]} method       'GET/POST/PUT/DELETE'
+     * @param  {[type]} url          '/example/url'. Url that we get from the http request object.
+     */
+    router.executeCallback = function (request, response, async) {
+        var method = request.method,
+            url = request.url;
+        if(!requests[method]) {
+            throw new RouterException('HTTP Method not found.');
+        }
+        else {
+            var callBackToBeExecuted = (!async)? requests[method].callbacks[url] : requests[method].asyncCallbacks[url];
+
+            if(!callBackToBeExecuted) {
+                throw new RouterException('HTTP Method with url "' + url + '" was not found.', 'pageNotFoundException');
+            }
+            callBackToBeExecuted(request, response);
+        }
+    }
+
+    return router;
+
+}());
+
+
+module.exports = router;
---
+++ week0/chirper/walter/walter.js
@@ -0,0 +1,133 @@
+var http = require('http');
+var router = require('./router');
+// require json parser
+// require https
+// require config parser
+
+var walter = (function() {
+    'use strict';
+
+    var walter = {};
+
+    ///////////////////////////////////
+    // Generic Walter Exception Def  //
+    ///////////////////////////////////
+
+    function ServerException (message, innerType, internalMessage) {
+        this.type = 'ServerException';
+        this.innerType = innerType || 'innerServerException';
+        this.message = message;
+        this.internalMessage = internalMessage || null;
+    }
+
+    //////////////////////
+    // Helper Functions //
+    //////////////////////
+
+    function isFullRequest (flag) {
+        if(flag || flag === undefined) {
+            return true;
+        }
+        if(flag === false) {
+            return false;
+        }
+        else {
+            throw new ServerException('Incorrect request flag. Flag Value:' + flag);
+        }
+    }
+
+    /**
+     * Converts a json object to an array of values without the keys
+     * @param  {JSON} jsonObj
+     * @return {argsArray}
+     */
+    function convertJsonToArguments (jsonObj) {
+        return Object.keys(jsonObj).map(function (key) {return jsonObj[key]});
+    }
+
+    function returnErrorResponse (response, statusCode, error) {
+        response.writeHead(statusCode, error.message, {'Content-Type': 'text/html'});
+        response.end();
+    }
+
+    ////////////////////////
+    // Walter Routing API //
+    ////////////////////////
+
+    walter.get = function (url, callback, waitForFullRequest) {
+        if(isFullRequest(waitForFullRequest)) {
+            router.get(url, callback);
+        }
+        else {
+            router.getAsync(url, callback);
+        }
+    }
+
+    walter.post = function (url, callback, waitForFullRequest) {
+        if(isFullRequest(waitForFullRequest)) {
+            router.post(url, callback);
+        }
+        else {
+            router.postAsync(url, callback);
+        }
+    }
+
+    walter.put = function (url, callback, waitForFullRequest) {
+        if(isFullRequest(waitForFullRequest)) {
+            router.put(url, callback);
+        }
+        else {
+            router.putAsync(url, callback);
+        }
+    }
+
+    walter.delete = function (url, callback, waitForFullRequest) {
+        if(isFullRequest(waitForFullRequest)) {
+            router.delete(url, callback);
+        }
+        else {
+            router.deleteAsync(url, callback);
+        }
+    }
+
+    walter.process = function (request, response, async) {
+        try {
+            router.executeCallback(request, response, async);
+        }
+        catch(err) {
+            console.log(err.type);
+            console.log(err.message);
+            console.log(err.internalMessage);
+            if(err.innerType === 'pageNotFoundException') {
+                returnErrorResponse(response, 404, "Page not found.");
+            }
+            else {
+                returnErrorResponse(response, 500, "Could not handle that Error: " + err.message);
+            }
+        }
+    }
+
+    walter.serve = function (port) {
+        var server = http.createServer(function (request, response) {
+            var payload = '';
+            request.on('data', function (chunk) {
+                payload += chunk;
+                request.payload = chunk;
+                walter.process(request, response, true);
+            });
+
+            request.on('end', function () {
+                request.payload = payload;
+                walter.process(request, response, false);
+            });
+        });
+
+        server.listen(port);
+    }
+
+    return walter;
+
+}());
+
+
+module.exports = walter;
