7cad79ddeb3e58bb3f7243f5b226fe6c32c2b3ca
dobata <dkralev90@gmail.com>
2014-10-18 14:40:07 +0300

    Adding HackerNews API calls module

--- week1/hackernews-scraper/scraper/api-calls.js
+++ week1/hackernews-scraper/scraper/api-calls.js
@@ -1,36 +1,69 @@
 var calls = (function() {
     'use strict';
+    var requestMod = require('request');
 
     // Hacker News API url and routes
     // Description: https://github.com/HackerNews/API
-    var hn = {
-        apiUrl: 'https://hacker-news.firebaseio.com',
-        item: '/v0/item/', // stories, comments, jobs
-        user: '/v0/user/', // users
-        topstories: '/v0/topstories', //last 100 stories
-        maxitem: '/v0/maxitem', // latest article
-        updates: '/v0/updates' // changed items, user profiles
-    }
+    var hn = {};
+    hn.apiUrl = 'https://hacker-news.firebaseio.com',
+    hn.item = hn.apiUrl + '/v0/item/{{id}}.json?print=pretty', // stories, comments, jobs
+    hn.user = hn.apiUrl +  '/v0/user/{{id}}.json?print=pretty', // users
+    hn.topStories = hn.apiUrl + '/v0/topstories.json?print=pretty', //last 100 stories
+    hn.maxitem = hn.apiUrl + '/v0/maxitem.json?print=pretty', // latest article
+    hn.updates = hn.apiUrl + '/v0/updates.json?print=pretty' // changed items, user profiles
 
     var calls = {};
 
-
-    calls.getArticle = function (articleId) {
-
+    calls.testConnectivity = function () {
+        requestMod( hn.apiUrl, function (error, response, html) {
+            if (!error && response.statusCode === 200) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        });
     }
 
-    calls.getTopArtilces = function () {
+    calls.getArticle = function (articleId) {
+        requestMod({
+            uri: hn.item.replace('{{id}}', articleId),
+            method: "GET",
+        }, function(error, response, body) {
+            console.log(body);
+        });
+    }
 
+    calls.getTopArticles = function () {
+        requestMod({
+            uri: hn.topStories,
+            method: "GET",
+        }, function(error, response, body) {
+            console.log(body);
+        });
     }
 
     calls.getMaxItem = function () {
-
+        requestMod({
+            uri: hn.maxitem,
+            method: "GET",
+        }, function(error, response, body) {
+            console.log(body);
+        });
     }
 
     calls.getUpdates =function () {
-
+        requestMod({
+            uri: hn.updates,
+            method: "GET",
+        }, function(error, response, body) {
+            console.log(body);
+        });
     }
 
     return calls;
 
 }());
+
+
+module.exports = calls;
