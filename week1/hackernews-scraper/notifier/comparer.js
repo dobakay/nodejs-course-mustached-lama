013350f3df6b27c0bf833f37c824ebbdc9598f3e
dobata <dkralev90@gmail.com>
2014-10-26 22:54:48 +0200

    Rewriting comparer of HN notifier to filter by userTypePreferences

--- week1/hackernews-scraper/notifier/comparer.js
+++ week1/hackernews-scraper/notifier/comparer.js
@@ -3,6 +3,7 @@
 
     var localStorage = require('../localStorage');
     var hnItems = localStorage.items; // hacker news items from API
+    var subscribers = localStorage.subscribers;
 
     var comparer = {};
 
@@ -13,50 +14,75 @@
         return result;
     }
 
-    function filterNewItemsByUserPreferences (userTypePreferences) {
+    function generateFeedList () {
+        var userItemsMatches = {};
+
+        for (var subKey in subscribers) {
+            userItemsMatches[subKey] = getItemsByUserPreferences(subscribers[subKey]);
+            // send mails via another module
+        }
+    }
+
+    function getItemsByUserPreferences (user) {
+        var filteredByTypeItems = getItemsByUserTypePreferences(user.type);
+        var filteredByKeySetItems = getItemsByUserKeyWordsPreferences(user,filteredByTypeItems);
+        return filteredByKeySetItems;
+    }
+
+    function getItemsByUserTypePreferences (userTypeTypePreferences) {
         var filtered = {};
-        for (var i = 0; i < userTypePreferences.length; i++) {
-            filtered[userTypePreferences[i]] = filterItemsByType(userTypePreferences[i])
+        for (var i = 0; i < userTypeTypePreferences.length; i++) {
+            filtered[userTypeTypePreferences[i]] = filterItemsByType(userTypeTypePreferences[i])
         }
         return filtered;
     }
 
-    function getUserSets (id) {
-        return localStorage.subscribers[id].keyWordsSets;
+    function getItemsByUserKeyWordsPreferences (user, items) {
+        var filteredItems = {};
+        var itemKeywordsMatchIndexes = null;
+        for (var type in items) {
+            for (var itemIndex = 0; itemIndex < items[type].length; itemIndex++) {
+                itemKeywordsMatchIndexes = checkItemKeywordSets(items[type][itemIndex], user);
+                if(itemKeywordsMatchIndexes) {
+                    if(!filteredItems[type]) {
+                        filteredItems[type] = [];
+                    }
+                    filteredItems[type].push({
+                        matchedUser: user,
+                        matchedItem: item,
+                        matchIndexes: itemKeywordsMatchIndexes
+                    });
+                    // important: it's possible we fill our matches with with empty matchIndex arrays
+                    // so we must 'nullafy' ... heh what a funny word
+                    // or matchesIndexes before we continue the looping
+                    itemKeywordsMatchIndexes = null;
+                }
+            }
+        }
+        return filteredItems;
     }
 
-    function isArticleInteresting (articleId, userKeywordSet) {
-        for (var i = 0; i < userKeywordSet.length; i++) {
-            if( localStorage.items[articleId].title.indexOf(userKeywordSet[i])) {
+    function checkItemKeywordSets (item, user) {
+        var keywordSetIndexes = [];
+        for (var i = 0; i < user.keyWordsSets.length; i++) {
+            if(itemContainsKeywords(item, user.keyWordsSets[i])) {
+                keywordSetIndexes.push(i);
+            }
+        }
+        return keywordSetIndexes;
+    }
+
+    function itemContainsKeywords (item, kewordsSet) {
+        var fieldToCheck = (item.type === 'comment')? 'text': 'title';
+        for (var i = 0; i < kewordsSet.length; i++) {
+            if(item[fieldToCheck].indexOf(kewordsSet[i]) !== -1) {
                 return true;
             }
         }
         return false;
     }
 
-    function filterArticlesByKeywordSets (userId) {
-        var userSets = getUserSets(userId);
-        var interestingArticles = [];
-        for (var i = 0; i < userSets.length; i++) {
-            for (var articleKey in artciles) {
-                if( isArticleInteresting(articleKey, userSets[i])) {
-                    interestingArticles.push(artciles[articleKey]);
-                }
-
-            }
-        }
-        return interestingArticles;
-    }
-
-    function generateInterestedSubs () {
-        var userArticleMatches = {};
-        for (var subKey in localStorage.subscribers) {
-            userArticleMatches[subKey] = filterArticlesByKeywordSets(subKey);
-        }
-        return userArticleMatches;
-    }
-
-    function attachMails (collection) {
+        function attachMails (collection) {
         for(var key in collection) {
             collection[key].email = localStorage.subscribers[key].email;
         }
