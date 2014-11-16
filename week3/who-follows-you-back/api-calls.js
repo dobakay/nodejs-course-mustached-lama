d6f72a199977874f8268992c2d0be98d4a0a5a1f
dobata <dkralev90@gmail.com>
2014-11-16 19:34:01 +0200

    Adding empty api-module to graph implementations

---
+++ week3/who-follows-you-back/api-calls.js
@@ -0,0 +1,17 @@
+var apiCalls = (function() {
+    'use strict';
+    var request = require('request');
+    var github = {};
+
+
+    var calls = {};
+
+    calls.getFollowing = function (user) {
+    }
+
+    return calls;
+
+}());
+
+
+apiCalls.getFollowing('putzmitt3l');
